e6a129ef8d342289a3a28d23a22099f5e9ca82c2
Mohammed Ashraf <ashraf@Mohammeds-MacBook-Air.local>
2026-04-05 01:33:03 +0530

    sample gemini get api

--- src/main/java/com/example/demo/ChatController.java
+++ src/main/java/com/example/demo/ChatController.java
@@ -5,6 +5,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -22,6 +23,11 @@ public class ChatController {
     public ResponseEntity<String> chat(){
         String response = aiService.getAIResponse("Give five models of Gemini");
         return ResponseEntity.ok(response);
+    }
+
+    @GetMapping("/dad-jokes")
+    public String getDadJokes(@RequestParam( value = "message", defaultValue = "give me a dad joke") String message){
+        return aiService.getAIResponse(message);
     }
 
 }
